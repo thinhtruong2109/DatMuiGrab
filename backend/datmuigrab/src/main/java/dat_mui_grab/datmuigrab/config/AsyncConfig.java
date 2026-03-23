396e4e6f76f0c1b919a0e93895f0d774c3424860
thinhtruong2109 <truongquangthinh21092004@gmail.com>
2026-03-23 16:18:03 +0700

    fix some bugs

--- backend/datmuigrab/src/main/java/dat_mui_grab/datmuigrab/config/AsyncConfig.java
+++ backend/datmuigrab/src/main/java/dat_mui_grab/datmuigrab/config/AsyncConfig.java
@@ -1,11 +1,26 @@
 package dat_mui_grab.datmuigrab.config;
 
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
 @EnableScheduling
 public class AsyncConfig {
+
+	@Bean(name = "taskExecutor")
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(4);
+		executor.setMaxPoolSize(8);
+		executor.setQueueCapacity(200);
+		executor.setThreadNamePrefix("app-async-");
+		executor.initialize();
+		return executor;
+	}
 }
